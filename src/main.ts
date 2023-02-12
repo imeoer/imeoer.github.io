f8ec704d8e194ab9b622ddccfbbd54076674725a
w568w <1278297578@qq.com>
2023-02-12 11:14:53 +0800

    remove: unnecessary resources

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
-import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/brands.css';
 import './styles/index.less';
