ccc182cdf32674b15ee4682b79b104b651436050
w568w <1278297578@qq.com>
2023-02-12 11:05:32 +0800

    change: rewrite the whole project with vite and modern libraries

--- app.js
+++
@@ -1,2 +0,0 @@
-require('./css/index.css')
-require('font-awesome/css/font-awesome.css')
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import './styles/index.less';
